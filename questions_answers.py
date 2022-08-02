a79bf45f8cc82265b0931661921c5e8b0d37d4cb
krishthakar2 <78512937+krishthakar2@users.noreply.github.com>
2022-08-03 03:37:23 +1200

    Create questions_answers.py

---
+++ questions_answers.py
@@ -0,0 +1,15 @@
+questions = {"Q1. Which country has the highest poverty rate?": 'A',   # key has a question which is being asked with the associated value
+             "Q2. Which country has the lowest poverty rate?": 'D',
+             "Q3. Which racial demographic has the highest likelihood of living in poverty?": 'D',
+             "Q4. What age group has the highest poverty rate in the US?": 'B',
+             "Q5. Out of 100 people in a village how many people wouldn't have clean water": 'C',
+             "Q6. Out of 100 people in a village how many people would live on 2$ or less?": 'D',
+             "Q7. What is one of the biggest causes of poverty? ": 'A',
+             }
+answers = [["A. South Sudan", "B. India ", "C. London  ", "D. United States"],  # 2d list to hold all the possible questions
+           ["A. China", "B. India", "C. New Zealand", "D. Iceland"],
+           ["A. White", "B. African American", "C. Indian", "D. Native American"],
+           ["A. Adults", "B. Children", "C. Elderly", "D. Infant"],
+           ["A. 33", "B. 16", "C. 18", "D. 0"],
+           ["A. 23", "B. 15", "C. 88", "D. 53"],
+           ["A. Lack of jobs", "B. Climate Change", "C. Over population", "D. Inflation "], ]
